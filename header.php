995f9e7827d19dab6ea4db2edeba60cb16b5f25c
Eduardo-Vitesse <edu.dev.py@gmail.com>
2021-10-21 14:29:47 -0300

    first commit

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./favicon.png" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>HCI Investimentos</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <nav>
+                <a href="#" class="logo">
+                    <img src="./img/brand/logo-header.png" alt="Logo HCI">
+                </a>
+                <ul>
+                    <li><a href="#">Quem somos</a></li>
+                    <li><a href="#">Nossas soluções</a></li>
+                    <li><a href="#">Equipe</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+                <button class="btn-success">Abra sua conta</button>
+            </nav>
+        </div>
+    </header>
