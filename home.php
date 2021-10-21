23c8524b7a1d7087318aa640030bba816b61b3ef
Eduardo-Vitesse <edu.dev.py@gmail.com>
2021-10-21 20:08:48 -0300

    section hero desktop

--- home.php
+++ home.php
@@ -1,6 +1,11 @@
 <?php require_once('./header.php'); ?>
 <section class="s-hero">
-
+    <div class="container">
+        <div class="text-hero">
+            <h1>Assessoria financeira transparente e com visão de longo prazo</h1>
+            <p>Na HCI, construímos o futuro dia a dia. Primamos por soluções inovadoras, excelência em todas as nossas operações e relações duradouras com os clientes.</p>
+        </div>
+    </div>
 </section>
 
 <?php require_once('./footer.php'); ?>
